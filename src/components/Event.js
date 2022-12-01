866721ad019dd26db1fa23bb658eece7ecd4e597
jay <jayfct@gmail.com>
2022-12-01 16:06:44 +0800

    add event

--- src/components/Event.js
+++ src/components/Event.js
@@ -5,33 +5,35 @@ const Event = () => {
   let hostName;
   let placeID;
 
-  fetch("/event", {
-    headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoia2l0IiwidXNlcklkIjoia0k4c2F6T0hqXyIsInJlZ2lzdGVyVGltZSI6IjIwMjItMTEtMTRUMDk6NDY6MTEuMTU3WiIsImlhdCI6MTY2ODQxOTI0M30.rJ5k9LG36A_F2c9s2QsaQqVjQOwhoqzb7q5T8wqGu3A",
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify({
-      eventName: "nana dancing",
-      username: "nana",
-      placeId: "p01",
-      maxNumOfTeamMember: 5,
-      startTime: Date.now(),
-      startPoint: (47.651, -122.349, 4326),
-      endPoint: (47.651, -122.349, 4326),
-      path: 1,
-      difficulty: "easy",
-      distance: 1,
-      description: "1",
-      isFinish: false,
-      hikingTime: 1,
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+  const handleCreateEvent = () => {
+    fetch("/event", {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoia2l0IiwidXNlcklkIjoia0k4c2F6T0hqXyIsInJlZ2lzdGVyVGltZSI6IjIwMjItMTEtMTRUMDk6NDY6MTEuMTU3WiIsImlhdCI6MTY2ODQxOTI0M30.rJ5k9LG36A_F2c9s2QsaQqVjQOwhoqzb7q5T8wqGu3A",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        eventName: "coding",
+        username: "jayjay",
+        placeId: "p02",
+        maxNumOfTeamMember: 5,
+        startTime: new Date(),
+        startPoint: { x: 23.3129033, y: 115.2180816 },
+        endPoint: { x: 22.3129033, y: 114.2180816 },
+        //  path: LINESTRING(1 1,2 3,4 8, -6 3),
+        difficulty: "hard",
+        distance: 1,
+        description: "1",
+        isFinish: false,
+        hikingTime: 1,``
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
   return (
     <div>
@@ -76,23 +78,45 @@ const Event = () => {
       </div>
 
       <div className="input-start-point">
-        <span> Start point</span>
-        <input
-          type="text"
-          onChange={(e) => {
-            eventName = e.target.value;
-          }}
-        />
+        <div className="input-start-point-x">
+          <span> Start point X</span>
+          <input
+            type="text"
+            onChange={(e) => {
+              eventName = e.target.value;
+            }}
+          />
+        </div>
+        <div className="input-start-point-x">
+          <span> Start point Y</span>
+          <input
+            type="text"
+            onChange={(e) => {
+              eventName = e.target.value;
+            }}
+          />
+        </div>
       </div>
 
       <div className="input-end-point">
-        <span> End point</span>
-        <input
-          type="text"
-          onChange={(e) => {
-            eventName = e.target.value;
-          }}
-        />
+        <div className="input-end-point-x">
+          <span> End point X</span>
+          <input
+            type="text"
+            onChange={(e) => {
+              eventName = e.target.value;
+            }}
+          />
+        </div>
+        <div className="input-end-point-x">
+          <span> End point Y</span>
+          <input
+            type="text"
+            onChange={(e) => {
+              eventName = e.target.value;
+            }}
+          />
+        </div>
       </div>
 
       <div className="select-page">
@@ -111,6 +135,8 @@ const Event = () => {
           <option value="p04">Maclehose Trail</option>
         </select>
       </div>
+
+      <button onClick={handleCreateEvent}>Create event</button>
     </div>
   );
 };
